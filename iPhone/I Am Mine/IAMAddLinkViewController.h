c4752d8f02675a9d75b9f5736028d8c2ef06b337
Giacomo Tufano <gt@iltofa.it>
2013-03-06 15:07:39 +0100

    Started Add Link

---
+++ iPhone/I Am Mine/IAMAddLinkViewController.h
@@ -0,0 +1,15 @@
+//
+//  IAMAddLinkViewController.h
+//  I Am Mine
+//
+//  Created by Giacomo Tufano on 06/03/13.
+//  Copyright (c) 2013 Giacomo Tufano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IAMAddLinkViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *linkEditor;
+
+@end
