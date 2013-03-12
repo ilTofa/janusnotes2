19762ae189954a875f6decd9618d01bc5e851ab9
Giacomo Tufano <gt@iltofa.it>
2013-03-12 15:54:31 +0100

    Changed base colors.

--- iPhone/I Am Mine/GTThemer.m
+++ iPhone/I Am Mine/GTThemer.m
@@ -33,21 +33,21 @@
         sharedInstance = [[GTThemer alloc] init];
         sharedInstance.colorsConfigs = @[
                                          @{
-                                             @"textColor" : [UIColor blackColor],
-                                             @"backgroundColor" : [UIColor colorWithWhite:0.950 alpha:1.000],
-                                             @"tintColor" : [UIColor colorWithWhite:0.667 alpha:1.000]},
-                                         @{
-                                             @"textColor" : [UIColor lightGrayColor],
-                                             @"backgroundColor" : [UIColor darkGrayColor],
-                                             @"tintColor" : [UIColor darkGrayColor]},
+                                             @"textColor" : [UIColor colorWithRed:0.118 green:0.000 blue:0.000 alpha:1.000],
+                                             @"backgroundColor" : [UIColor colorWithWhite:0.850 alpha:1.000],
+                                             @"tintColor" : [UIColor colorWithWhite:0.655 alpha:1.000]},
                                          @{
                                              @"textColor" : [UIColor blackColor],
                                              @"backgroundColor" : [UIColor whiteColor],
-                                             @"tintColor" : [UIColor colorWithWhite:0.667 alpha:1.000]},
+                                             @"tintColor" : [UIColor colorWithWhite:0.700 alpha:1.000]},
+                                         @{
+                                             @"textColor" : [UIColor colorWithRed:1.000 green:1.000 blue:0.969 alpha:1.000],
+                                             @"backgroundColor" : [UIColor colorWithRed:0.000 green:0.188 blue:0.318 alpha:1.000],
+                                             @"tintColor" : [UIColor colorWithRed:0.169 green:0.318 blue:0.420 alpha:1.000]},
                                          @{
                                              @"textColor" : [UIColor colorWithRed:0.216 green:0.212 blue:0.192 alpha:1.000],
                                              @"backgroundColor" : [UIColor colorWithRed:1.000 green:0.988 blue:0.922 alpha:1.000],
-                                             @"tintColor" : [UIColor colorWithRed:0.502 green:0.251 blue:0.000 alpha:1.000]},
+                                             @"tintColor" : [UIColor colorWithHue:0.083 saturation:1.000 brightness:0.502 alpha:1.000]},
                                          ];
         sharedInstance.fontsConfigs = @[@"Cochin", @"Georgia", @"Helvetica", @"Marker Felt"];
         [sharedInstance getDefaultValues];
