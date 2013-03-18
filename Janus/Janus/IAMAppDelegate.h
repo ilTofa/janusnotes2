4b5a01644ea4bbfea4425edad38a74ce0b39b730
Giacomo Tufano <gt@iltofa.it>
2013-03-18 10:35:24 +0100

    Starting OSX version (janus) - with first icon

---
+++ Janus/Janus/IAMAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  IAMAppDelegate.h
+//  Janus
+//
+//  Created by Giacomo Tufano on 18/03/13.
+//  Copyright (c) 2013 Giacomo Tufano. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IAMAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
