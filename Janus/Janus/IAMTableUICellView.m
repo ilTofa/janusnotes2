d90cc87323bdecf8fe2b01746af5ee5641d5f6c8
Giacomo Tufano <gt@iltofa.it>
2013-04-22 19:01:14 +0200

    Main UI with table view

---
+++ Janus/Janus/IAMTableUICellView.m
@@ -0,0 +1,28 @@
+//
+//  IAMTableUICellView.m
+//  Janus
+//
+//  Created by Giacomo Tufano on 22/04/13.
+//  Copyright (c) 2013 Giacomo Tufano. All rights reserved.
+//
+
+#import "IAMTableUICellView.h"
+
+@implementation IAMTableUICellView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
---
+++ Janus/Janus/IAMTableUIWindowController.h
@@ -0,0 +1,17 @@
+//
+//  IAMTableUIWindowController.h
+//  Janus
+//
+//  Created by Giacomo Tufano on 22/04/13.
+//  Copyright (c) 2013 Giacomo Tufano. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IAMTableUIWindowController : NSWindowController
+
+@property (weak, atomic) NSManagedObjectContext *sharedManagedObjectContext;
+@property (assign) IBOutlet NSArrayController *arrayController;
+@property (assign) IBOutlet NSTableView *theTable;
+
+@end
