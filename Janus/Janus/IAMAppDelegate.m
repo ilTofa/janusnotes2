cb6e9a810fd6b4af65eeb1f4de329de688ef211a
Giacomo Tufano <gt@iltofa.it>
2013-07-03 00:34:22 +0200

    Fixed a bug in openFile management.

--- Janus/Janus/IAMAppDelegate.m
+++ Janus/Janus/IAMAppDelegate.m
@@ -66,6 +66,7 @@
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1.0 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
             [self openFile:filename];
         });
+        retValue = YES;
     }
     return retValue;
 }
