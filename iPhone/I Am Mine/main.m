0a24040f5f3dc2993d7c7b73f7e0b6e7b37a69f2
Giacomo Tufano <gt@iltofa.it>
2013-08-31 10:28:31 +0200

    Fixes Xcode 5 IOKit assertmacro output.

--- iPhone/I Am Mine/main.m
+++ iPhone/I Am Mine/main.m
@@ -10,8 +10,24 @@
 
 #import "IAMAppDelegate.h"
 
+// TODO: remove hack on final release!
+
+typedef int (*PYStdWriter)(void *, const char *, int);
+static PYStdWriter _oldStdWrite;
+
+int __pyStderrWrite(void *inFD, const char *buffer, int size)
+{
+    if ( strncmp(buffer, "AssertMacros:", 13) == 0 ) {
+        return 0;
+    }
+    return _oldStdWrite(inFD, buffer, size);
+}
+
+
 int main(int argc, char *argv[])
 {
+    _oldStdWrite = stderr->_write;
+    stderr->_write = __pyStderrWrite;
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([IAMAppDelegate class]));
     }
