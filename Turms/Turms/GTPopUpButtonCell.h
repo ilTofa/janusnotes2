682e17dc0e124a360af317b035fc9655184345b9
Giacomo Tufano <gt@iltofa.it>
2014-04-24 10:10:58 +0200

    PopUp menu in toolbar for Publish.

---
+++ Turms/Turms/GTPopUpButtonCell.h
@@ -0,0 +1,13 @@
+//
+//  GTPopUpButtonCell.h
+//  Turms
+//
+//  Created by Giacomo Tufano on 24/04/14.
+//  Copyright (c) 2014 Giacomo Tufano. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GTPopUpButtonCell : NSPopUpButtonCell
+
+@end
---
+++ Turms/Turms/GTPopUpButtonCell.m
@@ -0,0 +1,40 @@
+//
+//  GTPopUpButtonCell.m
+//  Turms
+//
+//  Created by Giacomo Tufano on 24/04/14.
+//  Copyright (c) 2014 Giacomo Tufano. All rights reserved.
+//
+
+#import "GTPopUpButtonCell.h"
+
+@implementation GTPopUpButtonCell
+
+- (void)drawImageWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
+{
+    // draw image with modified rect for left-aligning
+    [super drawImageWithFrame:cellFrame inView:controlView];
+    
+    // draw the triangle to the bottom/right of image
+    NSRect imageBounds = [self imageRectForBounds:cellFrame];
+    NSBezierPath *path = [[NSBezierPath alloc] init];
+    [path moveToPoint:NSMakePoint(NSMaxX(imageBounds)-1, NSMaxY(imageBounds)-9)];
+    [path lineToPoint:NSMakePoint(NSMaxX(imageBounds)+5, NSMaxY(imageBounds)-9)];
+    [path lineToPoint:NSMakePoint(NSMaxX(imageBounds)+2, NSMaxY(imageBounds)-5)];
+    [path closePath];
+    
+    [[NSColor colorWithDeviceWhite:20.0/255.0 alpha:0.9] set];
+    [path fill];
+}
+
+- (NSRect)imageRectForBounds:(NSRect)theRect
+{
+    NSRect rect = [super imageRectForBounds:theRect];
+    
+    // make room for 5 pixels at the right
+    rect.origin.x -= 5;
+    
+    return rect;
+}
+
+@end
