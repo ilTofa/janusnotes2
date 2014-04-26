16bc250307550228086168708bb72d4e0d6ffed9
Giacomo Tufano <gt@iltofa.it>
2014-04-27 01:13:30 +0200

    Drop in the edit window creates a link (or an image link).

--- Turms/Turms/IAMNoteEditTextView.m
+++ Turms/Turms/IAMNoteEditTextView.m
@@ -27,9 +27,20 @@
 
 -(BOOL)performDragOperation:(id<NSDraggingInfo>)sender {
     NSPasteboard *pb = [sender draggingPasteboard];
+    DLog(@"Pasteboard contains: %@", [pb types]);
     // Intercept file URLS and cut the cache directory
     if ( [[pb types] containsObject:@"public.file-url"] ) {
         NSString *urlString = [pb propertyListForType:@"public.file-url"];
+        CFStringRef fileExtension = (CFStringRef) CFBridgingRetain([urlString pathExtension]);
+        CFStringRef fileUTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, fileExtension, NULL);
+        if (UTTypeConformsTo(fileUTI, kUTTypeImage)) {
+            DLog(@"It's an image");
+            urlString = [NSString stringWithFormat:@"![](%@)", urlString];
+        } else if (UTTypeConformsTo(fileUTI, kUTTypeText)) {
+            DLog(@"It's text");
+            urlString = [NSString stringWithFormat:@"[](%@)", urlString];
+        }
+        CFRelease(fileUTI);
         NSString *retVal = [urlString stringByReplacingOccurrencesOfString:self.cacheDirectory withString:@"$attachment$!"];
         [pb setString:retVal forType:NSPasteboardTypeString];
     }
