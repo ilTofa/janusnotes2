92af8b69dbf5e348894fa15c4d064ba65e84246d
Giacomo Tufano <gt@iltofa.it>
2014-05-03 10:42:58 +0200

    Fixed javascript bookmarklet

--- common/js/openInTurms.js
+++ common/js/openInTurms.js
@@ -10,8 +10,6 @@ open('x-turms:' + encodeURIComponent(+ document.URL + '?' + document.title + '?'
 
 javascript:(function(){open('x-turms:'+encodeURIComponent(document.URL)+'?'+encodeURIComponent(document.title)+'?'+encodeURIComponent(document.getSelection().toString())).focus();})();
 
-javascript:(function(){open('x-turms:'+encodeURIComponent(document.URL)+'?'+encodeURIComponent(document.title)+'?'+encodeURIComponent(document.selection.createRange().toString())).focus();})();
-
 <A HREF="javascript:(function(){var i,x; for (i=0;x=document.links[i];++i)x.style.color=[%22blue%22,%22red%22,%22orange%22][sim(x,location)]; function sim(a,b) { if (a.hostname!=b.hostname) return 0; if (fixPath(a.pathname)!=fixPath(b.pathname) || a.search!=b.search) return 1; return 2; } function fixPath(p){ p = (p.charAt(0)==%22/%22 ? %22%22 : %22/%22) + p;/*many browsers*/ p=p.split(%22?%22)[0];/*opera*/ return p; } })()" 
  title="This is a bookmarklet click to use, drag it to your links toolbar to use elsewhere"><span class="cyan">int/ext links</span></A>&quot;&nbsp;
 (<font color="red">any browser</font>)
