6f763261f6396fc6abf0d33384d77d24c7aba763
Giacomo Tufano <gt@iltofa.it>
2014-05-16 19:29:05 +0200

    Imported IAP UI from iOS (not tested).

--- Turms/Turms/IAMPrefsWindowController.h
+++ Turms/Turms/IAMPrefsWindowController.h
@@ -12,8 +12,13 @@
 
 @property NSFont *currentFont;
 @property (weak) IBOutlet NSTextField *encryptionKeyField;
+@property (weak) IBOutlet NSTextField *productCoffeePriceLabel;
+@property (weak) IBOutlet NSButton *productCoffeeLabel;
+@property (weak) IBOutlet NSButton *restoreCellLabel;
 
 - (IBAction)actionChangeFont:(id)sender;
 - (IBAction)changeEncryptionKey:(id)sender;
+- (IBAction)restorePurchase:(id)sender;
+- (IBAction)removeAds:(id)sender;
 
 @end
